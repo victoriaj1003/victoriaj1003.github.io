d95d92f006e1df1a701dfbee1e5c955d324017ee
victoriaj13@gmail.com <victoriaj13@gmail.com>
2016-02-11 12:10:27 -0500

    adding navbar things, alternating between hamburger and full nav not working on resize

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,51 @@
 
 
 function unhide() {
+	// check viewport width
+	var intViewportWidth = window.innerWidth;
+  
+  if (intViewportWidth < 526) {
+		document.getElementById('burger').style.visibility = 'visible';
+	document.body.style.overflow = "visible";
+  
+	document.getElementById('greeting').innerHTML = " i'm victoria";
+		$("#greeting").css({"cursor":"default",});
 
-  document.getElementById('navbar').style.visibility = 'visible';
+	} else {
+
+  	document.getElementById('navbar').style.visibility = 'visible';
   document.body.style.overflow = "visible";
   
   document.getElementById('greeting').innerHTML = " i'm victoria";
-  	$("#greeting").css("cursor", "default");
-  
+  	$("#greeting").css({"cursor":"default",});
+  	
+  }
+
 }
+
+
+
+
+
 
 $( document ).ready(function() {
 	console.log("ready freddy");
 
+	$( document ).resize(function() {
+    var screen = $(window)    
+    if (screen.width < 526) {
+        $("#burger").show();
+        $("#navbar").hide();
+        setInterval(resize, 5000);
+    } else {
+        $("#burger").hide();
+        $("#navbar").show();
+        setInterval(resize, 5000);
+    }
+
+});
 	
 
 
 });
+
