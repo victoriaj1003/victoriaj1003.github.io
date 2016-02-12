5e31e8651a55e3bf870d05e0ca3a8925009bbfc9
victoriaj13@gmail.com <victoriaj13@gmail.com>
2016-02-12 17:17:24 -0500

    fixed resize issue

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,38 @@
 function unhide() {
 	// check viewport width
 	var intViewportWidth = window.innerWidth;
-  
-  if (intViewportWidth < 526) {
-		document.getElementById('burger').style.visibility = 'visible';
+
+	// when hello is clicked, the menu appears, the scroll unlocks and the greeting changes
 	document.body.style.overflow = "visible";
-  
-	document.getElementById('greeting').innerHTML = " i'm victoria";
+	  
+	document.getElementById('greeting').innerHTML = "i'm victoria";
 		$("#greeting").css({"cursor":"default",});
 
-	} else {
-
-  	document.getElementById('navbar').style.visibility = 'visible';
-  document.body.style.overflow = "visible";
-  
-  document.getElementById('greeting').innerHTML = " i'm victoria";
-  	$("#greeting").css({"cursor":"default",});
-  	
-  }
-
+		document.getElementById('topbar').style.visibility = 'visible';
 }
 
+// 	// if hamburger is clicked on, list becomes visible
+// document.getElementById("burger").onclick = function() {dropMenu()};
+// 	console.log('dropMenu was found');
+
+// // dropdown toggles between show and hide
+// 	function dropMenu() {
+// 		document.getElementById("menuItems").classList.toggle("show");
+// 	}
+
+// 	// closes the menu if user clicks outside of it
+// 	window.onClick = function(event) {
+// 		if (!event.target.matches('#burger')) {
+// 			var dropdown = document.getElementByClassName("menu-content");
+// 			var i;
+// 			for (i = 0; i < dropdown.length; i++) {
+// 				var openDrop = dropdown[i];
+// 				if (openDrop.classList.contains('show')) {
+// 					openDrop.classList.remove('show');
+// 				}
+// 			}
+// 		}
+// 	}
 
 
 
@@ -31,20 +43,34 @@ function unhide() {
 $( document ).ready(function() {
 	console.log("ready freddy");
 
-	$( document ).resize(function() {
-    var screen = $(window)    
-    if (screen.width < 526) {
-        $("#burger").show();
-        $("#navbar").hide();
-        setInterval(resize, 5000);
-    } else {
-        $("#burger").hide();
-        $("#navbar").show();
-        setInterval(resize, 5000);
-    }
+	function sizeMatters() {
 
-});
-	
+		if($(window).width() < 526) {
+			$("#navbar").hide();
+			$("#burger").show();
+			console.log("burger showing");
+		} else {
+			$("#burger").hide();
+			$("#navbar").show();
+			console.log("navbar showing");
+		}
+	}
+
+	$(window).load(sizeMatters);
+	$(window).resize(sizeMatters);
+
+
+
+
+	// if($("#burger").hover()) {
+	// 	$("#menuitems").css('visibility, visible');
+	// 	console.log("i'm here");
+	// } else {
+	// 	$("#menuitems").css('visibility, hidden');
+	// 	console.log("i'm invisible!");
+	// }
+
+
 
 
 });
