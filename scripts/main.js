a54f14765692472882eedbe2c7c01ae82ce3a36b
victoriaj13@gmail.com <victoriaj13@gmail.com>
2016-02-22 22:04:01 -0500

    simplified page

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,29 @@
 
 
-function unhide() {
+// function unhide() {
 	// check viewport width
-	var intViewportWidth = window.innerWidth;
+	// var intViewportWidth = window.innerWidth;
 
 	// when hello is clicked, the menu appears, the scroll unlocks and the greeting changes
-	document.body.style.overflow = "visible";
+	// document.body.style.overflow = "visible";
 	// document.getElementById('bodywrapper').style.overflow = "visible";
 	  
-	document.getElementById('greeting').innerHTML = "i'm victoria";
-		$("#greeting").css({"cursor":"default",});
+	// document.getElementById('greeting').innerHTML = "i'm victoria";
+	// 	$("#greeting").css({"cursor":"default",});
 
-		document.getElementById('topbar').style.visibility = 'visible';
-}
+		// document.getElementById('topbar').style.visibility = 'visible';
+// }
 
 
 
 $( document ).ready(function() {
 	console.log("ready freddy");
 
+	// $(window).load() 
+		// $("#greeting2").fadeIn(6000);
+		// 	console.log("greeting faded in");
+		
+	
 	function sizeMatters() {
 
 		if($(window).width() < 523) {
